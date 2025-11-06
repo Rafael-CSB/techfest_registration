0712b9a028c918eea3a37dc8f54efb0852d9054e
Rafael-CSB <rafael.alcantara@benilde.edu.ph>
2025-11-06 08:51:02 +0800

    Added Collect Participant Information

--- techfest_registration.py
+++ techfest_registration.py
@@ -6,3 +6,14 @@ if num_parti < 1:
     print("Invalid Input; Exiting Program...")
     exit()
 
+participants = []
+for i in range(num_parti):
+    print("\nParticipant", i+1, ":")
+    name = input("Enter Participant Name: ")
+    track = input("Enter Chosen Track: ")
+    participant = {"name": name, "track": track}
+    participants.append(participant)
+
+print("\nRegistered Participants:")
+for i in range(len(participants)):
+    print(str(i+1) + ".", participants[i]["name"], "-", participants[i]["track"])
