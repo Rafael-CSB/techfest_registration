c44f77e05fdef24421af565f08ff0c8966cfc48b
Rafael-CSB <rafael.alcantara@benilde.edu.ph>
2025-11-06 08:39:08 +0800

    Implemented participant registration

---
+++ techfest_registration.py
@@ -0,0 +1,8 @@
+print("Welcome to SMIT TechFest!")
+print("Event Organised by Rafael Alcantara of APPDAET BTCS1")
+
+num_parti = int(input("\nEnter the number of Participants: "))
+if num_parti < 1:
+    print("Invalid Input; Exiting Program...")
+    exit()
+
